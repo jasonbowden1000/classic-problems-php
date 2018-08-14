3664169b713f5d47d95761173c3fac2d2a64865a
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-14 11:09:26 -0500

    modernized array syntax in spec

--- spec/Problems/SearchSpec.php
+++ spec/Problems/SearchSpec.php
@@ -8,7 +8,7 @@ use Prophecy\Argument;
 
 class SearchSpec extends ObjectBehavior
 {
-    const SORTED_ARRAY = array(-13, -7, -7, 8, 8, 8, 8, 8, 8, 23, 33, 54, 91, 144, 1984, 1984, 2001);
+    const SORTED_ARRAY = [-13, -7, -7, 8, 8, 8, 8, 8, 8, 23, 33, 54, 91, 144, 1984, 1984, 2001];
 
     function it_should_perform_binary_search_on_sorted_array()
     {

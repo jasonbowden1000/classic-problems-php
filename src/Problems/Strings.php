1ce1852298bcbbf19789918aaec617ea6d62d769
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-15 23:15:11 -0500

    simplified palindrome

--- src/Problems/Strings.php
+++ src/Problems/Strings.php
@@ -6,27 +6,8 @@ class Strings
 {
     public function isPalindrome($str)
     {
-        $i = 0;
-        $j = strlen($str) - 1;
-        $result = true;
-
-        while ($i < $j) {
-            while (!ctype_alpha($str[$i]) && $i < $j) {
-                $i += 1;
-            }
-
-            while (!ctype_alpha($str[$j]) && $i < $j) {
-                $j -= 1;
-            }
-
-            if (strcasecmp($str[$i], $str[$j]) !== 0) {
-                $result = false;
-            }
-
-            $i += 1;
-            $j -= 1;
-        }
-
-        return $result;
+        $letters = preg_replace('/[^a-zA-Z]/', '', $str);
+        $letters = strtolower($letters);
+        return $letters === strrev($letters);
     }
 }

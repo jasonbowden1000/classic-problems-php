ea00a7e5cc02832b311799b21989f3b3f351c3ba
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-19 21:49:06 -0500

    roman numerals

--- spec/Problems/StringsSpec.php
+++ spec/Problems/StringsSpec.php
@@ -19,4 +19,53 @@ class StringsSpec extends ObjectBehavior
         $this->isPalindrome("axyza")->shouldBe(false);
         $this->isPalindrome("A car, a man, a maraca")->shouldBe(true);
     }
+
+    function it_should_convert_roman_numerals_to_arabic()
+    {
+        $this->romanToArabic('MDCCCLXVI')->shouldReturn(1866);
+        $this->romanToArabic('XIV')->shouldReturn(14);
+        $this->romanToArabic('LXXXIX')->shouldReturn(89);
+        $this->romanToArabic('XCI')->shouldReturn(91);
+        $this->romanToArabic('DCCCXC')->shouldReturn(890);
+        $this->romanToArabic('MCMLXXXIX')->shouldReturn(1989);
+    }
+
+    function it_should_test_for_valid_roman_numerals()
+    {
+         $this->isRoman('XXXX')->shouldBe(false);
+         $this->isRoman('LL')->shouldBe(false);
+         $this->isRoman('DDC')->shouldBe(false);
+         $this->isRoman('XVV')->shouldBe(false);
+         $this->isRoman('IX')->shouldBe(true);
+         $this->isRoman('XC')->shouldBe(true);
+         $this->isRoman('XL')->shouldBe(true);
+         $this->isRoman('CD')->shouldBe(true);
+         $this->isRoman('CM')->shouldBe(true);
+         $this->isRoman('IC')->shouldBe(false);
+         $this->isRoman('LC')->shouldBe(false);
+         $this->isRoman('VX')->shouldBe(false);
+         $this->isRoman('DM')->shouldBe(false);
+         $this->isRoman('CDM')->shouldBe(false);
+         $this->isRoman('IXC')->shouldBe(false);
+         $this->isRoman('IVX')->shouldBe(false);
+         $this->isRoman('XIXIX')->shouldBe(false);
+         $this->isRoman('XVIV')->shouldBe(false);
+         $this->isRoman('XIIV')->shouldBe(false);
+         $this->isRoman('VIX')->shouldBe(false);
+         $this->isRoman('XXC')->shouldBe(false);
+         $this->isRoman('CCCXCCC')->shouldBe(false);
+         $this->isRoman('LXL')->shouldBe(false);
+         $this->isRoman('IXI')->shouldBe(false);
+         $this->isRoman('IXXXI')->shouldBe(false);
+         $this->isRoman('XXX')->shouldBe(true);
+         $this->isRoman('XIV')->shouldBe(true);
+         $this->isRoman('CXC')->shouldBe(true);
+         $this->isRoman('XIX')->shouldBe(true);
+    }
+
+    function it_should_convert_arabic_numerals_to_roman()
+    {
+        $this->arabicToRoman(1666)->shouldBe('MDCLXVI');
+        $this->arabicToRoman(91)->shouldBe('XCI');
+    }
 }
--- src/Problems/Strings.php
+++ src/Problems/Strings.php
@@ -4,10 +4,66 @@ namespace Problems;
 
 class Strings
 {
+    const romanMap = [
+      "I" => 1,
+      "IV" => 4,
+      "V" => 5,
+      "IX" => 9,
+      "X" => 10,
+      "XL" => 40,
+      "L" => 50,
+      "XC" => 90,
+      "C" => 100,
+      "CD" => 400,
+      "D" => 500,
+      "CM" => 900,
+      "M" => 1000
+    ];
+
+    public function arabicToRoman($arabic)
+    {
+        $roman = '';
+        $map = array_reverse(self::romanMap);
+
+        foreach ($map as $k => $v) {
+            while ($arabic >= $v) {
+                $roman = $roman . $k;
+                $arabic -= $v;
+            }
+        }
+
+        return $roman;
+    }
+
     public function isPalindrome($str)
     {
         $letters = preg_replace('/[^a-zA-Z]/', '', $str);
         $letters = strtolower($letters);
         return $letters === strrev($letters);
+    }
+
+    public function isRoman($n)
+    {
+        $re = '/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/';
+        return preg_match($re, $n) === 1;
+    }
+
+    public function romanToArabic($roman)
+    {
+        $arabic = 0;
+
+        $arr = array_map(function($v) {
+            return self::romanMap[$v];
+        }, str_split($roman));
+
+        $arrLength = count($arr) - 1;
+
+        $arabic += $arr[$arrLength];
+
+        for ($i = $arrLength - 1; $i >= 0; --$i) {
+            $arabic += $arr[$i] >= $arr[$i + 1] ? $arr[$i] : -$arr[$i];
+        }
+
+        return $arabic;
     }
 }

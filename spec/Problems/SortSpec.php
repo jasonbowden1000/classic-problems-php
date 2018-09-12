27e0afb75a93fecf47ffb3ac7ca2d6d3ce1a5787
Jason Bowden <jason.bowden1000@gmail.com>
2018-09-12 17:23:25 -0500

    Pruned sorting tests

--- spec/Problems/SortSpec.php
+++ spec/Problems/SortSpec.php
@@ -10,21 +10,11 @@ class SortSpec extends ObjectBehavior
 {
   function it_should_return_an_ordered_array_when_merging_sorted_arrays()
   {
-      $arr1 = [5, 13, 17];
+      $arr1 = [5, 7, 13, 13, 17];
       $arr2 = [3, 7, 11, 19];
-      $merged = [3, 5, 7, 11, 13, 17, 19];
+      $merged = [3, 5, 7, 7, 11, 13, 13, 17, 19];
       $this->sortedMerged($arr1, $arr2)->shouldEqual($merged);
       $this->sortedMerged($arr2, $arr1)->shouldEqual($merged);
       $this->sortedMerged($arr1, $arr2)->shouldNotEqual([1, 2, 3, 4, 5, 6, 7]);
-  }
-
-  function it_should_return_an_ordered_array_when_given_sorted_arrays_with_duplicates()
-  {
-
-      $arr1 = [5, 5, 6, 13, 13, 13, 17];
-      $arr2 = [3, 6, 7, 11, 19, 19];
-      $merged = [3, 5, 5, 6, 6, 7, 11, 13, 13, 13, 17, 19, 19];
-      $this->sortedMerged($arr1, $arr2)->shouldEqual($merged);
-      $this->sortedMerged($arr1, $arr2)->shouldNotEqual([3, 5, 6, 7, 11, 13, 17, 19]);
   }
 }

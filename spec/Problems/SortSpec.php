031490d42ac64593e709d33a650d1409d8ffcdf3
Jason Bowden <jason.bowden1000@gmail.com>
2018-09-22 18:34:57 -0500

    Find intersection of two sorted arrays

--- spec/Problems/SortSpec.php
+++ spec/Problems/SortSpec.php
@@ -17,4 +17,12 @@ class SortSpec extends ObjectBehavior
       $this->sortedMerged($arr2, $arr1)->shouldEqual($merged);
       $this->sortedMerged($arr1, $arr2)->shouldNotEqual([1, 2, 3, 4, 5, 6, 7]);
   }
+
+  function it_should_return_a_sorted_array_when_finding_the_intersection_of_two_sorted_arrays()
+  {
+      $arr1 = [2, 2, 3, 3, 5, 5, 6, 7, 7, 8, 12];
+      $arr2 = [2, 2, 5, 6, 6, 8, 8, 9, 10];
+      $intersection = [2, 5, 6, 8];
+      $this->intersectSorted($arr1, $arr2)->shouldEqual($intersection);
+  }
 }
--- src/Problems/Sort.php
+++ src/Problems/Sort.php
@@ -4,6 +4,29 @@ namespace Problems;
 
 class Sort
 {
+    public function intersectSorted(array $a, array $b)
+    {
+        $i = 0;
+        $j = 0;
+        $intersection = [];
+        $length1 = count($a);
+        $length2 = count($b);
+
+        while ($i < $length1 && $j < $length2) {
+          if (($a[$i] === $b[$j]) && ($i === 0 || $a[$i] !== $a[$i - 1])) {
+            $intersection[] = $a[$i];
+            ++$i;
+            ++$j;
+          } else if ($a[$i] < $b[$j]) {
+            ++$i;
+          } else {
+            ++$j;
+          }
+        }
+
+        return $intersection;
+    }
+
     public function sortedMerged(array $arr1, array $arr2)
     {
         $merged = [];
